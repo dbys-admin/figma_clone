a693b690af72875ad254ffa4dd50626759fe2379
sjyda <sjyda90@gmail.com>
2024-06-21 17:54:54 -0700

    Fifth time commit. Finished up to Color Settings

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
 import { useEffect, useRef, useState } from "react";
-import { handleCanvaseMouseMove, handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
-import { ActiveElement } from "@/types/type";
+import { handleCanvaseMouseMove, handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvasObjectScaling, handleCanvasSelectionCreated, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
+import { ActiveElement, Attributes } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
@@ -27,8 +27,19 @@ export default function Page() {
 
   const imageInputRef = useRef<HTMLInputElement>(null);
 
+  const isEditingRef = useRef(false);
 
   const canvasObjects = useStorage((root)=> root.canvasObjects)
+
+  const [elementAttributes, setElementAttributes] = useState<Attributes>({
+    width:"",
+    height: "",
+    fontSize:"",
+    fontFamily:"",
+    fontWeight:"",
+    fill:"#aabbcc",
+    stroke:"#aabbcc"
+  })
 
   const syncShapeInStorage = useMutation(({storage}, object)=>{
     if(!object) return;
@@ -112,6 +123,19 @@ export default function Page() {
       })
     })
 
+    canvas.on("selection:created", (options: any)=> {
+      handleCanvasSelectionCreated({
+        options, isEditingRef, setElementAttributes
+      })
+    })
+
+    canvas.on("object:scaling", (options:any)=>{
+      handleCanvasObjectScaling({
+        options, setElementAttributes, 
+      })
+    })
+
+
 
     window.addEventListener("resize", ()=>{
       handleResize({fabricRef})
@@ -160,7 +184,12 @@ export default function Page() {
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
         <Live canvasRef={canvasRef} />
-        <RightSidebar />
+        <RightSidebar 
+          elementAttributes={elementAttributes}
+          setElementAttributes = {setElementAttributes}
+          fabricRef={fabricRef}
+          isEditingRef={isEditingRef}
+        />
       </section>
     </main>
   );
